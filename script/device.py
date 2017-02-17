79be8c3ccda209dbc9545b559046b787d0cdb9a3
Bright <Bright130@hotmail.com>
2017-02-17 20:51:21 +0700

    modified database structure

--- script/device.py
+++ script/device.py
@@ -1,143 +1,57 @@
+import csv
 import json
-columnDependencies = [16,17,18,19]   #if you want to add new property field please add index of column to this array(start from zero)
-columnArray = [1,2,9,13]
-columnNotArray = [0,7,8,10,11,12,14,15,20,21]
-columnProperties = [3,4,5,6]
-def newData(header):
-    newDataVal = {}
-    for ca in columnArray:
-        newDataVal[header[ca]]=[]
-    for cna in columnNotArray:
-        newDataVal[header[cna]]=""
-    newDataVal["Dependency"] = []
-    newDataVal["Property"] = []
-    return newDataVal
+import collections
+import sys
 
+if __name__ == '__main__':
+    input_filename = 'device.csv'
+    output_filename = 'device.json'
 
-def addDependencies(line, header):
-    dependencies = {}
-    for c in columnDependencies:
-        if line[c] != '':
-            dependencies[header[c]] = line[c] 
-    return dependencies
+    # Temporary variable to store data to be written to the json file
+    data = []
 
-def addProperties(line, header):
-    properties = {}
-    for c in columnProperties:
-        if line[c] != '':
-            properties[header[c]] = line[c] 
-    return properties
+    with open(input_filename, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        device = None
+        compatibility = None
+        properties = None
+        try:
+            for row in reader:
+                # We have found a new trigger so we create a dict and append to the correct category
+                if len(row['id']) != 0:
+                    device = collections.OrderedDict([('id', row['id']),
+                                                      ('lib_name', row['lib_name']),
+                                                      ('compatibility', {}),
+                                                      ('brand', row['brand']),
+                                                      ('model', row['model']),
+                                                      ('ic', row['ic']),
+                                                      ('price', row['price']),
+                                                      ('detail', row['detail']),
+                                                      ('image', row['image']),
+                                                      ('connect', [row['connect']]),
+                                                      ('min_volt', row['min_volt']),
+                                                      ('max_volt', row['max_volt']),
+                                                      ('dependency', []),
+                                                      ('datasheet', row['datasheet']),
+                                                      ('link', row['link'])])
+                    data.append(device)
 
-f = open("device.csv", "r")
-output = open("device.json", "w")
-text = f.readline()
-text = f.readline()
-output.write(" [\n")
-headers = text.split("\n")
-header = headers[0].split(",")
-#print header
-count = 0
-isArray = 0
-isDependency = 0
-dependency = {}
-isProperty = 0
-properties = {}
-data = newData(header)
-#print data
-while text != '':
+                if len(row['type_fn']) != 0:
+                    compatibility = {}
+                    device['compatibility'][row['type_fn']] = compatibility
 
-    text = f.readline()
-    if text == '':
-        break
-    #print text
-    line = text.split("\n")
-    
-    line = line[0].split(",")
-   
-    #print ','
-    #add json array
-    if line[0]=='':
-        
-        count = 0
-        for col in line:
-            isArray=0
-            isDependency=0
-            isProperty=0
-            if col!='':
-                for c in columnDependencies:
-                    if(count == c):
-                        isDependency = 1
-                for c in columnProperties:
-                    if(count == c):
-                        isProperty = 1                      
-                if (isDependency == 0)&(isProperty == 0):
-                    isArray=0
-                    for ca in columnArray :
-                        if count==ca :
-                            isArray=1
-                    #print isArray
-                    if isArray == 1:
-                        data[header[count]].append(col)
-                    else:
-                        data[header[count]] = col
-                    isArray=0
-            count = count +1  
-    
-        #print line  
-        dependencies = addDependencies(line, header)
-        if dependencies != {}:
-            data["Dependency"].append(dependencies)
-        properties = addProperties(line, header)
-        if properties != {}:
-            data["Property"].append(properties)  
+                if len(row['id_fn']) != 0:
+                    compatibility[row['id_fn']] = row['fn_name']
 
-    elif line[0]!='' :
-        count=0
-      
-       #normal line  
-        if data!= newData(header):
-            json.dump(data, output, indent=4, separators=(',', ': '))
-            output.write(",")
-        #print data["Dependency"]
-        
-        data = newData(header)
-        for col in line:
-            isArray=0
-            isDependency=0 
-            isProperty=0
-            if col!='':
-                for c in columnDependencies:
-                    if(count == c):
-                        isDependency = 1
-                for c in columnProperties:
-                    if(count == c):
-                        isProperty = 1                    
-                if (isDependency == 0)&(isProperty == 0):
-                    
-                    for ca in columnArray :
-                        if count==ca :
-                            isArray=1
-                    
-                    if isArray == 1:
-                        data[header[count]].append(col)
-                    else:
-                        data[header[count]] = col
-                    
-            count = count +1 
-        #print line
-        dependencies = addDependencies(line,header)
-        if(dependencies!={}) :
-            data["Dependency"].append(dependencies)
-        properties = addProperties(line,header)
-        if(properties!={}) :
-            data["Property"].append(properties)
-              
-  
-if data!={}:
-  
-   json.dump(data, output, indent=4, separators=(',', ': '))
-       
+                if len(row['depend_type']) != 0:
+                    properties = collections.OrderedDict([('type', row['depend_type']),
+                    								      ('min', row['depend_min']),
+                    								      ('max', row['depend_max']),
+                    								      ('unit', row['depend_unit'])]) 
+               	    device['dependency'].append(properties)
+        except csv.Error as e:
+            # Exit and display error massage if the input CSV file is invalid
+            sys.exit('file {}, line {}: {}'.format(input_filename, reader.line_num, e))
 
-f.close()
-output.write(" ]\n")
-output.close()
+    with open(output_filename, 'w') as csvfile:
+        json.dump(data, csvfile, indent=2)
