4f34875a0f410e2819a72a6f763fc894756d2bba
Nititorn <nititornsun@gmail.com>
2017-07-18 13:13:02 +0700

    New actualdevice python and format input, output device python

--- newscript/actualdevice.py
+++ newscript/actualdevice.py
@@ -3,99 +3,134 @@ import json
 import collections
 import sys
 
-def isfloat(value):
+
+def isfloat(tempfloat):
     try:
-        float(value)
-        return True
+        float(tempfloat)
+        return float(tempfloat)
     except ValueError:
-        return False
+        return tempfloat
+
 
 if __name__ == '__main__':
     input_filename = 'actualdevice.csv'
     output_filename = 'actualdevice.json'
-    
+
     # Temporary variable to store data to be written to the json file
     data = []
-
     with open(input_filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        obj = None
-        device = None
-        action = None
-        param = None
-        constraint = None
-        devicevalue = None
-        value = None
-        constraintvalue = None
-        # Add new variable for default action
-        #defaultaction = None
+        ports = []
+        compatibilities = []
+        dependencies = []
+        functions = []
+        actions = []
+        values = []
+        params = []
+        constraint = []
+        valueconstraint = []
         try:
             for row in reader:
-                # We have found a new category so we create an array and put it into the dict
-                if len(row['brand']) != 0:
-                    supportdevices = []
-                    supportdevicevalues = []
-                    obj = collections.OrderedDict([('brand', row['brand']),
-                                                   ('model', row['model']),
-                                                   ('url', row['url']),
-                                                   ('supportdevice', supportdevices),
-                                                   ('supportvalue', supportdevicevalues)])
-                    data.append(obj)
-                    
-                if len(row['supportdevice']) != 0:
+                # We have found a new category so we create an array and put it into the actualdevice
+                if len(row['id']) != 0:
+                    ports = []
+                    compatibilities = []
+                    dependencies = []
+                    actualdevice = collections.OrderedDict([('id', row['id']),
+                                                            ('brand', row['brand']),
+                                                            ('model', row['model']),
+                                                            ('url', row['url']),
+                                                            ('type', row['type']),
+                                                            ('formfactor', row['formfactor']),
+                                                            ('platform', []),
+                                                            ('width', isfloat(row['width'])),
+                                                            ('height', isfloat(row['height'])),
+                                                            ('port', ports),
+                                                            ('connectivity', []),
+                                                            ('compatibility', compatibilities),
+                                                            ('dependency', dependencies),
+                                                            ])
+                    data.append(actualdevice)
+
+                if len(row['platform']) != 0:
+                    actualdevice['platform'].append(row['platform'])
+
+                if len(row['portname']) != 0:
+                    functions = []
+                    port = collections.OrderedDict([('name', row['portname']),
+                                                    ('type', row['porttype']),
+                                                    ('function', functions),
+                                                    ('v_min', isfloat(row['v_min'])),
+                                                    ('v_max', isfloat(row['v_max'])),
+                                                    ('x', isfloat(row['x'])),
+                                                    ('y', isfloat(row['y']))])
+                    ports.append(port)
+
+                if len(row['functiontype']) != 0:
+                    tempfunction = collections.OrderedDict([('type', row['functiontype']),
+                                                            ('pintype', row['functionpintype'])])
+                    functions.append(tempfunction)
+
+                if len(row['connectivity']) != 0:
+                    actualdevice['connectivity'].append(row['connectivity'])
+
+                if len(row['compatibility']) != 0:
                     actions = []
-                    device = collections.OrderedDict([('name', row['supportdevice']),
-                                                      ('action', actions)])
-                    supportdevices.append(device)
-               
-                if len(row['supportaction']) != 0:
-                    params = []         
-                    action = collections.OrderedDict([('name', row['supportaction']),
+                    values = []
+                    compatibility = collections.OrderedDict([('name', row['compatibility']),
+                                                             ('action', actions),
+                                                             ('value', values),
+                                                             ('count', isfloat(row['count']))])
+                    compatibilities.append(compatibility)
+
+                if len(row['action']) != 0:
+                    params = []
+                    action = collections.OrderedDict([('name', row['action']),
                                                       ('parameter', params)])
                     actions.append(action)
 
-                if len(row['supportactionparam']) != 0:
-                    constraints = []
-                    param = collections.OrderedDict([('name', row['supportactionparam']),
-                                                     ('constraint', constraints)])
+                if len(row['param']) != 0:
+                    constraint = []
+                    if len(row['unit']) != 0:
+                        constraint = collections.OrderedDict([('min', isfloat(row['min'])),
+                                                              ('max', isfloat(row['max'])),
+                                                              ('unit', row['unit'])])
+                    param = collections.OrderedDict([('name', row['param']),
+                                                     ('constraint', constraint)])
                     params.append(param)
 
-                if len(row['constraintunit']) != 0:
-                    constraint = collections.OrderedDict([('min', float(row['constraintmin'])),
-                                                          ('max', float(row['constraintmax'])),
-                                                          ('unit', row['constraintunit'])])
-                    constraints.append(constraint)
+                if len(row['constraint']) != 0:
+                    constraint.append(row['constraint'])
 
-                if len(row['constraintvalue']) !=0:
-                    constraints.append(row['constraintvalue'])
-
-                if len(row['supportdevicevalue']) !=0:
-                    values = []
-                    devicevalue = collections.OrderedDict([('name', row['supportdevicevalue']),
-                                                           ('value', values)])
-                    supportdevicevalues.append(devicevalue)
-
-                if len(row['supportvalue']) !=0:
-                    constraintvalues = []
-                    value = collections.OrderedDict([('name', row['supportvalue']),
-                                                     ('constraint', constraintvalues)])
+                if len(row['value']) != 0:
+                    valueconstraint = []
+                    if len(row['valueunit']) != 0:
+                        valueconstraint = collections.OrderedDict([('min', isfloat(row['valuemin'])),
+                                                                   ('max', isfloat(row['valuemax'])),
+                                                                   ('unit', row['valueunit'])])
+                    value = collections.OrderedDict([('name', row['value']),
+                                                     ('constraint', valueconstraint)])
                     values.append(value)
 
-                if len(row['constraintvalueunit']) != 0:
-                    constraintvalue = collections.OrderedDict([('min', float(row['constraintvaluemin'])),
-                                                          ('max', float(row['constraintvaluemax'])),
-                                                          ('unit', row['constraintvalueunit'])])
-                    constraintvalues.append(constraintvalue)
+                if len(row['valueconstraint']) != 0:
+                    valueconstraint.append(row['valueconstraint'])
 
-                if len(row['constraintvaluevalue']) !=0:
-                    constraintvalues.append(row['constraintvaluevalue'])
-                    
+                if len(row['dependency']) != 0:
+                    dependency = collections.OrderedDict([('name', row['dependency']),
+                                                          ('device', [])])
+                    dependencies.append(dependency)
+
+                if len(row['device']) != 0:
+                    dependency['device'].append(row['device'])
 
                 # Raise error if no valid information is found in any column
-                if ((len(row['brand']) == 0) and (len(row['supportdevice']) == 0) and (len(row['supportaction']) == 0)
-                    and (len(row['supportactionparam']) == 0) and (len(row['constraintunit']) == 0) and (len(row['constraintvalue']) == 0)
-                    and (len(row['supportdevicevalue']) == 0) and (len(row['supportvalue']) == 0)
-                    and (len(row['constraintvalueunit']) == 0) and (len(row['constraintvaluevalue']) == 0)):
+                if ((len(row['id']) == 0) and (len(row['platform']) == 0)
+                    and (len(row['portname']) == 0) and (len(row['connectivity']) == 0)
+                    and (len(row['compatibility']) == 0) and (len(row['action']) == 0)
+                    and (len(row['param']) == 0) and (len(row['unit']) == 0)
+                    and (len(row['constraint']) == 0) and (len(row['value']) == 0)
+                    and (len(row['valueconstraint']) == 0) and (len(row['dependency']) == 0)
+                    and (len(row['device']) == 0)):
                     raise csv.Error('No valid data is detected at this line!!!')
         except csv.Error as e:
             # Exit and display error massage if the input CSV file is invalid
--- newscript/genericinputdevice.py
+++ newscript/genericinputdevice.py
@@ -3,26 +3,30 @@ import json
 import collections
 import sys
 
+
+def isfloat(tempfloat):
+    try:
+        float(tempfloat)
+        return float(tempfloat)
+    except ValueError:
+        return tempfloat
+
+
 if __name__ == '__main__':
     input_filename = 'genericinputdevice.csv'
     output_filename = 'genericinputdevice.json'
-    
+
     # Temporary variable to store data to be written to the json file
     data = []
-
     with open(input_filename, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
-        obj = None
-        action = None
-        action_paramname = None
-        tempconstraint = None
-        value = None
-        valueconstraint = None
-        # Add new variable for default action
-        #defaultaction = None
+        inputs = []
+        values = []
+        constraints = []
+        valueconstraints = []
         try:
             for row in reader:
-                # We have found a new category so we create an array and put it into the dict
+                # We have found a new category so we create an array and put it into the obj
                 if len(row['name']) != 0:
                     inputs = []
                     values = []
@@ -30,61 +34,51 @@ if __name__ == '__main__':
                                                    ('action', inputs),
                                                    ('value', values)])
                     data.append(obj)
-                    
-                # We have found a new action so we create a dict and append to the correct category
+
                 if len(row['action_name']) != 0:
                     action = collections.OrderedDict([('name', row['action_name']),
                                                       ('funcname', row['fn_name']),
                                                       ('type', row['action_type']),
                                                       ('parameter', [])])
-                    # Check is default action
-                    #if row['defaultaction'] == 'YES':
-                    #    obj['defaultAction'] = row['action_name']
                     inputs.append(action)
-               
-                # If a parameter is found, we append it to the current action. If an argument or a                
-                # default value is found, we append it the the current parameter
+
                 if len(row['action_paramname']) != 0:
                     constraints = []
-
                     if row['action_paramtype'] == 'DOUBLE' or row['action_paramtype'] == 'INTEGER':
-                        defaultValue = float(row['action_default'])
-                        constraints = collections.OrderedDict([('min', float(row['action_constraintmin'])),
-                                                               ('max', float(row['action_constraintmax'])),
+                        defaultValue = isfloat(row['action_default'])
+                        constraints = collections.OrderedDict([('min', isfloat(row['action_constraintmin'])),
+                                                               ('max', isfloat(row['action_constraintmax'])),
                                                                ('unit', row['action_constraintunit'])])
                     elif row['action_paramtype'] == 'ENUM':
                         defaultValue = row['action_default']
                         constraints.append(row['action_constraintvalue'])
-
                     action_paramname = collections.OrderedDict([('name', row['action_paramname']),
-                                                            ('value', defaultValue),
-                                                            ('constraint', constraints),
-                                                            ('datatype', row['action_paramtype']),
-                                                            ('controltype', row['action_control'])])
-                    action['parameter'].append(action_paramname)  
+                                                                ('value', defaultValue),
+                                                                ('constraint', constraints),
+                                                                ('datatype', row['action_paramtype']),
+                                                                ('controltype', row['action_control'])])
+                    action['parameter'].append(action_paramname)
                 elif len(row['action_constraintvalue']) != 0:
                     constraints.append(row['action_constraintvalue'])
 
                 if len(row['value_name']) != 0:
                     valueconstraints = []
-                    
                     if row['value_datatype'] == 'DOUBLE' or row['value_datatype'] == 'INTEGER':
-                        valueconstraints = collections.OrderedDict([('min', float(row['value_constraintmin'])),
-                                                                   ('max', float(row['value_constraintmax'])),
-                                                                   ('unit', row['value_constraintunit'])])
+                        valueconstraints = collections.OrderedDict([('min', isfloat(row['value_constraintmin'])),
+                                                                    ('max', isfloat(row['value_constraintmax'])),
+                                                                    ('unit', row['value_constraintunit'])])
                     elif row['value_datatype'] == 'ENUM':
                         valueconstraints.append(row['value_constraintvalue'])
-
                     value = collections.OrderedDict([('name', row['value_name']),
-                                                   ('dataType', row['value_datatype']),
-                                                   ('constraint', valueconstraints)])
+                                                     ('dataType', row['value_datatype']),
+                                                     ('constraint', valueconstraints)])
                     values.append(value)
                 elif len(row['value_constraintvalue']) != 0:
                     valueconstraints.append(row['value_constraintvalue'])
-                    
+
                 # Raise error if no valid information is found in any column
-                if ((len(row['action_name']) == 0) and (len(row['action_paramname']) == 0) 
-                    and (len(row['action_constraintunit']) == 0) and (len(row['action_constraintvalue']) == 0) 
+                if ((len(row['action_name']) == 0) and (len(row['action_paramname']) == 0)
+                    and (len(row['action_constraintunit']) == 0) and (len(row['action_constraintvalue']) == 0)
                     and (len(row['value_name']) == 0) and (len(row['value_constraintunit']) == 0)
                     and (len(row['value_constraintvalue']) == 0)):
                     raise csv.Error('No valid data is detected at this line!!!')
