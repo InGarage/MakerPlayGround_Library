11a6a29b4e1beea86e7fb1481fdaa9752a9982a2
kchaiyo <9104050+kchaiyo@users.noreply.github.com>
2018-08-01 22:04:47 +0700

    Add device MP_0018

---
+++ device_list_generated_json/actualDevice/MP-0018/src/MP_MP_0018.cpp
@@ -0,0 +1,32 @@
+#include "MP_MP_0018.h"
+
+
+MP_MP_0018::MP_MP_0018(uint8_t echo ,uint8_t trig,const String &tag )
+  : trig(trig), echo(echo),tag(tag)
+{
+  
+}
+
+void MP_MP_0018::init()  
+{	
+	pinMode(this->trig,OUTPUT);
+	pinMode(this->echo,INPUT);
+	MP_Log::i(tag,"Ready");
+}
+
+double MP_MP_0018::getDistance() {	
+
+    long cm=0 ;
+    /* loop for filter an hardware error */
+    do {
+        digitalWrite(trig, LOW);
+        delayMicroseconds(2);
+        digitalWrite(trig, HIGH);
+        delayMicroseconds(10);
+        digitalWrite(trig, LOW);
+        cm = pulseIn(echo, HIGH) / 29.0 / 2.0 ;
+  	} while(cm>3000);
+  	MP_Log::i(tag,cm);
+    return cm;
+
+}
---
+++ device_list_generated_json/actualDevice/MP-0018/src/MP_MP_0018.h
@@ -0,0 +1,24 @@
+#ifndef MP_MP_0018_H
+#define MP_MP_0018_H
+
+#include <Arduino.h>
+#include "MP_Log.h"
+
+class MP_MP_0018
+{
+  public:
+
+	 MP_MP_0018(uint8_t echo ,uint8_t trig,const String &tag ) ;
+
+	 double getDistance() ;
+	 void init() ;
+
+  private:
+	  uint8_t trig;
+	  uint8_t echo;
+	  const String tag;
+	 
+
+};
+
+#endif
