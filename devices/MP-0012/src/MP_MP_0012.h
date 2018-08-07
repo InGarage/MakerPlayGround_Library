3f41d19fea59da076c7d50269eb89c378ad056c9
kchaiyo <9104050+kchaiyo@users.noreply.github.com>
2018-08-07 18:08:38 +0700

    MP_0012 replace SoftwareSerial with SendOnlySoftwareSerial

--- devices/MP-0012/src/MP_MP_0012.h
+++ devices/MP-0012/src/MP_MP_0012.h
@@ -2,12 +2,12 @@
 #define MP_MP_0012_H
 
 #include <Arduino.h>
-#include <SoftwareSerial.h>
+#include "SendOnlySoftwareSerial.h"
 
 class MP_MP_0012
 {
 public:
-	MP_MP_0012(uint8_t tx, uint8_t rx,const String &tag);
+	MP_MP_0012(uint8_t tx, const String &tag);
 
 	void init();
 	void showValue(double num);
@@ -18,8 +18,8 @@ private:
 	void sendInvalid();
     void showFloat(double num);
     void sendData(int num, uint8_t dot, bool negative);
+    SendOnlySoftwareSerial serial;
     const String tag;
-	SoftwareSerial serial;
 };
 
 #endif
